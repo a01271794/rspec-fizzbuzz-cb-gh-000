499c55d40b0fd8e908b69ad5737e35e5ecb84654
Oscar Frederick Perez Reyes <a01271794@itesm.mx>
2019-02-07 03:05:28 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,11 +2,11 @@
 # See README.md for instructions on how to do this
 def fizzbuzz (int)
   if int % 15 == 0
-    return"FizzBuzz"
+    puts"FizzBuzz"
   elsif int % 3 == 0
-    return"Fizz"
+    puts"Fizz"
   elsif int % 5 == 0
-    return"Buzz"
+    puts"Buzz"
   else
     return nil
 end
